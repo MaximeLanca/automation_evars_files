cb75dbb41bbc92503d22a5ad924abbe882f5a16f
MaximeLanca <lanca.maxime.ber@gmail.com>
2025-10-02 10:09:17 +0200

    Test commit

--- main.py
+++ main.py
@@ -8,36 +8,40 @@ from pathlib import Path
 PATH_ADMIN_FOLDER = 'C:\\AVEVA\\Administration\\2.1\\Administration\\'
 RELATIF_PATH = '%CD%'
 VARIABLE_TO_DETECTE = '%~dp0'
+#donne l'emplacement exact du script
 script_path = Path(__file__).resolve()
+#dossier contenant le script
 script_dir = script_path.parent
 
+def main():
+    get_trigram()
 
 def __init__(self):
     pass
 
 def search_customEvars():
-
+    pass
 
 #Fonction pour trouver le TRIgramme
 def get_trigram():
-    
     pattern = re.compile(r'^([A-Z]{3})')
-    return pattern
+    folder_names = [name for name in os.listdir(script_dir) if re.search(pattern,name)]
+    print(folder_names)
 
 def create_folders():
-    trigram_project = get_trigram()
+    pass
+    """ trigram_project = get_trigram()
     name_folder = trigram_project + 'load'
     name_folder.mkdir(parents=True, exist_ok = True)
 
     os.chdir(script_dir +'/'+name_folder )
-
+ """
     
 def read_admin_files():
     pass
 
 if __name__ == "__main__":
-    app = Main()
-    app.run()
+    main()
 
 
 # mettre le script avec evarsTRI
